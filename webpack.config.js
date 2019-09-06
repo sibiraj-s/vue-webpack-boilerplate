11377d7c99f1323a47b0e826e3ccce9ebdf1af00
sibiraj-s <dev.sibiraj@outlook.com>
2019-09-06 18:43:17 +0530

    feat: add support for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,12 @@ const devServer = require('./webpack/devServer');
 const optimization = require('./webpack/optimization');
 
 const Paths = require('./webpack/Paths');
+const pkgJson = require('./package.json');
 
 const WEBPACK_MODE = process.env.NODE_ENV === 'development' ? 'development' : 'production';
 const isProduction = WEBPACK_MODE === 'production';
 
-module.exports = {
+const config = {
   mode: WEBPACK_MODE,
   bail: isProduction,
   resolve: {
@@ -28,3 +29,10 @@ module.exports = {
   optimization,
   devServer,
 };
+
+// changes required to deploy in Github Pages
+if (process.env.NODE_ENV === 'gh-pages') {
+  config.output.publicPath = `/${pkgJson.name}/`;
+}
+
+module.exports = config;

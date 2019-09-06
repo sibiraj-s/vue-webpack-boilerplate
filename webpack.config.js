a51e64eb7330972a13c57fb6572669330ff7570e
sibiraj-s <dev.sibiraj@outlook.com>
2019-09-06 18:33:15 +0530

    feat: split node_modules as a seperate chunk

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const rules = require('./webpack/rules');
 const plugins = require('./webpack/plugins');
 const devServer = require('./webpack/devServer');
+const optimization = require('./webpack/optimization');
 
 const Paths = require('./webpack/Paths');
 
@@ -16,9 +17,14 @@ module.exports = {
     extensions: ['.js', '.vue'],
   },
   entry: path.resolve(Paths.srcDir, 'index.js'),
+  output: {
+    path: Paths.outDir,
+    publicPath: '/',
+  },
   module: {
     rules,
   },
   plugins,
+  optimization,
   devServer,
 };
--- webpack/rules.js
+++ webpack/rules.js
@@ -1,6 +1,7 @@
 module.exports = [
   {
     test: /\.js$/,
+    exclude: /(node_modules)/,
     loader: 'babel-loader',
   },
   {
